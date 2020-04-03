dab47d5a90173174bd4062879973315d8671bf3a
Dylan Rodriquez <dylanmr@uchicago.edu>
2020-04-03 14:47:59 -0400

    cuknn

--- knearest.py
+++ knearest.py
@@ -1,8 +1,19 @@
 import LoadData as ld
 import numpy as np
 from sklearn.model_selection import train_test_split
-
+from sklearn import neighbors
+from sklearn.metrics import confusion_matrix
+from cuml.neighbors import KNeighborsClassifier as cuKNN
    
 data, val, datalabel, vallabel = ld.trainvalsplit('train')
+
+k = 10
+knearest = cuKNN(n_neighbors = k)
+knearest.fit(data,datalabel)
+predictions = knearest.predict(val)
+
+
+
+
 
 
