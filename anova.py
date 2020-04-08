8006b843e404a2bb9a8226968f2eaddc6ca49ab2
dylan <dmrodriqu@gmail.com>
2020-04-08 01:47:12 -0400

    post hoc test bracket comparison

--- anova.py
+++ anova.py
@@ -1,5 +1,8 @@
 import numpy as np
+from math import erf
 from scipy.stats import f as F
+from scipy.stats import t as T
+from itertools import combinations
 
 
 # population mean
@@ -8,7 +11,7 @@ def mean(population):
     return popsum/len(population)
 # sum squares
 
-def sumSquares(concatenatedPopulation):
+def sumSquares(concatenatedPopulation, u):
     return sum([(sample - u)**2 for sample in concatenatedPopulation])
 
 # sum of squares residual
@@ -19,7 +22,7 @@ def sumSquaresResid(population, groupMeans):
     return ssresid
 
 # total sum of squares
-def totss(population, groupMeans):
+def totss(population, groupMeans, u):
     sse = 0
     ix = 0
     for i in population:
@@ -38,31 +41,79 @@ def createGroups(population):
 def calculatePValue(numbersamples, numbergroups, ssresid, ssexplained):
     df = numbersamples - numbergroups
     meansquaresres = ssresid/df
-
     dfex = numbergroups - 1
-    meansquaresEx = sse/dfex
-
+    meansquaresEx = ssexplained/dfex
     fScore = meansquaresEx/meansquaresres
     pval = 1- F.cdf(fScore, dfex, df)
-    
     return meansquaresres, meansquaresEx, fScore, pval
+
+
+# anova function to calculate 1 way anova
+# take in population as array of group values
+# outputs mean square residual, mean square of groups, group sum of squares
+# f score, and p value
+def anova(population):
+    popcon, groupMeans, u, numbergroups, numbersamples = createGroups(population)
+    ss = sumSquares(popcon, u)
+    ssresid = sumSquaresResid(population, groupMeans)
+    sse = totss(population, groupMeans , u)
+    msres, msex, fscore, p = calculatePValue(numbersamples, numbergroups, ssresid, sse)
+    return ss, ssresid, sse, msres, msex, fscore, p
+
 
 
 A = [12.6, 12, 11.8, 11.9, 13, 12.5, 14]
 B = [10, 10.2, 10, 12, 14, 13]
-C = [10.1, 13, 13.4, 12.9, 8.9, 10.7, 13.6, 12]
-population = np.array([np.array(A),np.array(B), np.array(C)])
+C = [0.1, 1, 1, 1.9, 8.9, 1.7, 1.6, 12]
+D = [0.1, 12, 13, 1.9, .9, 1.7, 1.6, 12]
 
-popcon, groupMeans, u, numbergroups, numbersamples = createGroups(population)
-ss = sumSquares(popcon)
-ssresid = sumSquaresResid(population, groupMeans)
-sse = totss(population, groupMeans)
-msres, msex, fscore, p = calculatePValue(numbersamples, numbergroups, ssresid, sse)
 
-print(ss)
-print(ssresid)
-print(sse)
-print(msres)
-print(msex)
-print(fscore)
-print(p)
+
+population =  [np.array(A), np.array(B), np.array(C), np.array(D)]
+def ttest(s1, s2):
+    # calculate means
+    smu1 = s1.mean()
+    smu2 = s2.mean()
+    # calcuate standard deviation
+    sigma1 = s1.std()
+    sigma2 = s2.std()
+    sigman1 = ((sigma1**2)/len(s1)) 
+    sigman2 = ((sigma2**2)/len(s2))
+    # get estimator
+    ssq = (sigman1 + sigman2)
+    s = ssq**(1/2)
+    #calculate degrees of freedom unequal variance
+    df  = (ssq**2)/(((sigman1**2)/(len(s1)-1)) + (sigman2**2)/(len(s2)-1))
+    # calculate t
+    t = (smu1 - smu2)/s
+    # difference != 0
+    oneTail = (T.cdf(abs(t), df=df))
+    alpha = 0.05
+    if (2*(1-oneTail)) < alpha:
+        # difference is > 0
+        if (1-oneTail) < alpha:
+            print(1-oneTail)
+            return 1
+        # difference is < 0
+        else:
+            print(oneTail)
+            return 2
+    return 0
+print(anova(population))
+
+def postHoc(population):
+    winners = []
+    losers = []
+    for i,j in combinations(range(len(population)), 2):
+        if (ttest(population[i],population[j])) == 1:
+            winners.append(population[i])
+        else:
+            losers.append(population[j])
+    if not bool(winners):
+        return postHoc(winners)
+    else:
+        return winners, losers
+print(postHoc(population)[0])
+
+        
+
