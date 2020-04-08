8a217ee047e9fbd2266bb66a8a6a334cb50dacaa
dylan <dmrodriqu@gmail.com>
2020-04-07 21:33:23 -0400

    anova

---
+++ anova.py
@@ -0,0 +1,70 @@
+import numpy as np
+from scipy.stats import f as F
+
+
+# population mean
+def mean(population):
+    popsum = sum(population)
+    return popsum/len(population)
+# sum squares
+
+def sumSquares(concatenatedPopulation):
+    return sum([(sample - u)**2 for sample in concatenatedPopulation])
+
+# sum of squares residual
+def sumSquaresResid(population, groupMeans):
+    ssresid = 0
+    for i in range(len(population)):
+        ssresid += sum((population[i] - groupMeans[i])**2)
+    return ssresid
+
+# total sum of squares
+def totss(population, groupMeans):
+    sse = 0
+    ix = 0
+    for i in population:
+        n = len(i)
+        means = np.repeat(groupMeans[ix],n)
+        sse += sum((means - u)**2)
+        ix += 1
+    return sse
+
+def createGroups(population):
+    groupMeans = [np.apply_along_axis(mean, 0, s) for s in population]
+    popcon = np.concatenate(population)
+    u = popcon.mean()
+    return popcon, groupMeans, u, len(groupMeans), len(popcon)
+
+def calculatePValue(numbersamples, numbergroups, ssresid, ssexplained):
+    df = numbersamples - numbergroups
+    meansquaresres = ssresid/df
+
+    dfex = numbergroups - 1
+    meansquaresEx = sse/dfex
+
+    fScore = meansquaresEx/meansquaresres
+    pval = 1- F.cdf(fScore, dfex, df)
+    
+    return meansquaresres, meansquaresEx, fScore, pval
+
+
+A = [12.6, 12, 11.8, 11.9, 13, 12.5, 14]
+B = [10, 10.2, 10, 12, 14, 13]
+C = [10.1, 13, 13.4, 12.9, 8.9, 10.7, 13.6, 12]
+population = np.array([np.array(A),np.array(B), np.array(C)])
+
+popcon, groupMeans, u, numbergroups, numbersamples = createGroups(population)
+ss = sumSquares(popcon)
+ssresid = sumSquaresResid(population, groupMeans)
+sse = totss(population, groupMeans)
+msres, msex, fscore, p = calculatePValue(numbersamples, numbergroups, ssresid, sse)
+
+print(ss)
+print(ssresid)
+print(sse)
+
+print(msres)
+print(msex)
+print(fscore)
+print(p)
+
