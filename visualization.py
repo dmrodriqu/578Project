5f824d34b79857ca8627ca1e4ecaf66b0c78fb2b
dylan <dmrodriqu@gmail.com>
2020-04-20 13:14:50 -0400

    indices used for file reading

--- visualization.py
+++ visualization.py
@@ -35,28 +35,66 @@ def processknn():
             cluster += 1
     return iters
 
-
-def boxplots(kn ,precision=False, digit = 0):
-    alldat = []
-    allpr = []
-    for i in kn:
-        accarr = []
-        prarr = []
-        if kn[i]:
-            acc = kn[i]
-            for j in acc:
-                if not precision:
-                    accarr.append(j[0])
-                else:
-                    prarr.append(j[1][digit])
-        alldat.append(accarr)
-        allpr.append(prarr)
-    fig, ax = plt.subplots(nrows=1, ncols = 1, figsize =(9,4))
-    if precision:
-        bpl = ax.boxplot(allpr, vert = True)
-    else:
-         bpl = ax.boxplot(alldat, vert = True)
-    plt.show()
-
+'''
 kn = processknn()
 boxplots(kn, precision = True, digit = 1)
+'''
+
+'''
+0 neighbors
+2 fold number
+4 dictionary tp/fp
+7 predictions
+10 labels
+difference between  neighbors = 55
+'''
+def structData(filename, atr):
+    attrb = {'neighbors': 0, 
+            'fold': 2,
+            'dict': 4,
+            'pred': 7,
+            'truth': 10}
+    f = open(filename, 'r')
+    s = f.read() 
+    f.close()
+    linelist = s.splitlines()
+    #print(linelist[55])
+    data  = []
+    for k in range(0,15):
+        if atr == 'pred' or 'truth':
+            folds = [list(map(int, linelist[attrb[atr] + (55*k) + (i*10)].split(','))) for i in range(0,5)]
+        else:
+            folds = [linelist[attrb[atr] + (55*k) + (i*10)] for i in range(0,5)]
+        data.append(folds)
+    return data
+
+'''
+mcc per folds
+'''
+def getMCC(labels, predictions):
+    predlen = len(predictions)
+    return ([[accuracy(labels[i][k], predictions[i][k]) for k in range(len(predictions[i]))] for i in range(predlen)])
+
+
+'''
+confusion matrix per fold
+'''
+def getconfusionMatrix(labels, predictions):
+    predlen = len(predictions)
+    return ([[confusionMatrix(labels[i][k], predictions[i][k]) for k in range(len(predictions[i]))] for i in range(predlen)])
+
+
+predictions =structData(outfiles[0], 'pred')
+labels = structData(outfiles[0], 'truth')
+
+cc = getconfusionMatrix(labels, predictions)
+print(np.shape(cc[0][1]))
+
+allmcc = getMCC(labels, predictions)
+fig, ax = plt.subplots(nrows=1, ncols = 1, figsize =(9,4))
+bpl = ax.boxplot(allmcc, vert = True)
+plt.show()
+
+
+    
+
